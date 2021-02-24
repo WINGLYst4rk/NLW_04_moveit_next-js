d7d9c074d6c4ed3f1b26bd40ddfe0ef184c13c03
WINGLYst4rk <wingly205@gmail.com>
2021-02-24 16:46:14 -0300

    update the countdown function

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
     const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
@@ -15,9 +17,15 @@ export function Countdown() {
         setIsActive(true);
     }
 
+    function resetCountdown() {
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(25 * 60);
+    }
+
     useEffect(() => {
         if (isActive && time > 0){
-            setTimeout(() => {
+            countdownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
         }
@@ -37,13 +45,24 @@ export function Countdown() {
                 </div>
             </div>
 
-            <button 
-                type="button" 
-                className={styles.countdownButton}
-                onClick={startCountdown}
-            >
-                Iniciar um ciclo
-            </button>
+            { isActive ? (
+                <button 
+                    type="button" 
+                    className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                    onClick={resetCountdown}
+                >
+                    Abandonar ciclo
+                </button>
+            ) : (
+
+                <button 
+                    type="button" 
+                    className={styles.countdownButton}
+                    onClick={startCountdown}
+                >
+                    Iniciar um ciclo
+                </button>
+            ) }
         </div>
     );
 }
