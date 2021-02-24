54cc604d7314a694fee06db5106f130401a5929f
WINGLYst4rk <wingly205@gmail.com>
2021-02-23 21:33:52 -0300

    adding the profile section

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
     return (
-        <div>
+        <div className= {styles.profilecontainer}>
             <img src="https://github.com/WINGLYst4rk.png" alt="Wingly Rodrigues" />
             <div>
                 <strong>Wingly Rodrigues</strong>
