bc6eec22ce1d6c3c38beb956686e7d362ed30a9c
WINGLYst4rk <wingly205@gmail.com>
2021-02-23 21:48:05 -0300

    adding files

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
     return (
-        <div className= {styles.profilecontainer}>
+        <div className= {styles.profileContainer}>
             <img src="https://github.com/WINGLYst4rk.png" alt="Wingly Rodrigues" />
             <div>
                 <strong>Wingly Rodrigues</strong>
