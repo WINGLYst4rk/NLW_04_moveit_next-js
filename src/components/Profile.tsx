e129fe4650f4294f2d37317f77e68ef913e2c444
WINGLYst4rk <wingly205@gmail.com>
2021-02-23 21:17:26 -0300

    create nextJs app and start to build the app

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/WINGLYst4rk.png" alt="Wingly Rodrigues" />
+            <div>
+                <strong>Wingly Rodrigues</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
