0cac8d13b3529fd9a9a6c065c4edf6c16d01ddc7
WINGLYst4rk <wingly205@gmail.com>
2021-02-24 16:11:54 -0300

    changing the varible for a bool type

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
-    const [time, setTime] = useState(1 * 60);
-    const [active, setActive] = useState(false);
+    const [time, setTime] = useState(25 * 60);
+    const [isActive, setIsActive] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -12,16 +12,16 @@ export function Countdown() {
     const [ secondsLeft, secondsRight ] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown() {
-        setActive(true);
+        setIsActive(true);
     }
 
     useEffect(() => {
-        if (active && time > 0){
+        if (isActive && time > 0){
             setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
         }
-    }, [active, time]);
+    }, [isActive, time]);
 
     return (
         <div>
