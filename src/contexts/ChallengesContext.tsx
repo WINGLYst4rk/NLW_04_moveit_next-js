9b9fa2252083d3db4a6a32931eb2322d17b48ef5
WINGLYst4rk <wingly205@gmail.com>
2021-02-24 22:32:43 -0300

    using context api for create the global function

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,3 +1,21 @@
-import { createContext } from 'react';
+import { createContext, useState, ReactNode } from 'react';
 
 export const ChallengesContext = createContext({});
+
+interface ChallengeProviderProps {// to make a prop a optinal use the '?'
+    children: ReactNode;
+}
+
+export function ChallengesProvider({ children }: ChallengeProviderProps) {
+    const [level, setLevel] = useState(1);
+
+    function levelUp() {
+      setLevel(level + 1);
+    }
+  
+    return (
+        <ChallengesContext.Provider value={{level, levelUp}}>
+            {children}
+        </ChallengesContext.Provider>
+    );
+}
