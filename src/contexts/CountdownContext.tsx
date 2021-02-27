3e40831838ed051b349831edc788d0051d02c2fc
WINGLYst4rk <wingly205@gmail.com>
2021-02-26 21:54:41 -0300

    create the interface for countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -1,8 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useState, useEffect } from 'react';
 import { ChallengesContext } from './ChallengesContext';
 
 interface CountdownContextData {
-
+    minutes: number;
+    seconds: number;
+    hasFinished: boolean;
+    isActive: boolean;
+    startCountdown: () => void;
+    resetCountdown: () => void;
 }
 
 interface CountdownProviderProps {
@@ -11,20 +16,49 @@ interface CountdownProviderProps {
 
 const CountdownContext = createContext({} as CountdownContextData);
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge } = useContext(ChallengesContext);
 
     const [time, setTime] = useState(0.1 * 60);
-    const [isActive, setActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
+    function startCountdown() {
+        setIsActive(true);
+    }
+
+    function resetCountdown() {
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(0.1 * 60);
+    }
+
+    useEffect(() => {
+        if (isActive && time > 0) {
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+    }, [isActive, time]);
+
 
     return (
         <CountdownContext.Provider
             value={{
-
+                minutes,
+                seconds,
+                hasFinished,
+                isActive,
+                startCountdown,
+                resetCountdown
             }}
         >
             {children}
