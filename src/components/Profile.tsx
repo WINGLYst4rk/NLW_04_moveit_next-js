e1e662fa616d844c214a5905b70faca3df1b8d63
WINGLYst4rk <wingly205@gmail.com>
2021-02-26 21:43:47 -0300

    start a new context for countdown

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
-        <div className= {styles.profileContainer}>
+        <div className={styles.profileContainer}>
             <img src="https://github.com/WINGLYst4rk.png" alt="Wingly Rodrigues" />
             <div>
                 <strong>Wingly Rodrigues</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { ChallengesContext } from './ChallengesContext';
+
+interface CountdownContextData {
+
+}
+
+interface CountdownProviderProps {
+    children: ReactNode;
+}
+
+const CountdownContext = createContext({} as CountdownContextData);
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.1 * 60);
+    const [isActive, setActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    return (
+        <CountdownContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </CountdownContext.Provider>
+    );
+}
